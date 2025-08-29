33947f35091e100a17c329efebb4aefac6501210
hearthdesign <hearthxdesign@gmail.com>
2025-08-30 00:11:41 +0200

    Create home.html template and update home view to render it

--- artoon2d/artoon2d_blog/views.py
+++ artoon2d/artoon2d_blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'artoon2d_blog/home.html')
+    return render(request, 'home.html', {})
