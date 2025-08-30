006220266150d3b1eca762b60bdb7f6c0211c9b0
hearthdesign <hearthxdesign@gmail.com>
2025-08-30 19:32:03 +0200

    Add Post model and admin configuration; create initial migration

--- artoon2d_blog/admin.py
+++ artoon2d_blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+'''Connection to Post model'''
+from .models import Post
+''' Admin Class to manage Post model in the admin interface '''
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'theme', 'created_at')
+    search_fields = ('title', 'content')
+    list_filter = ('theme', 'author')
 
-# Register your models here.
+admin.site.register(Post, PostAdmin)
--- artoon2d_blog/models.py
+++ artoon2d_blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    featured_image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+    theme = models.CharField(max_length=50)
+  
+    def __str__(self):
+	    return self.title + ' | ' + str(self.author)
+
+    
