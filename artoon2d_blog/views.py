ed7e2117251f41f69402c8694c75f85e19b432a4
hearthdesign <hearthxdesign@gmail.com>
2025-08-31 03:18:59 +0200

    Add initial migrations and models for taggit integration
    - Create initial migration files for the taggit app, defining the Tag and TaggedItem models.
    - Add unique constraints and indexes to TaggedItem for better data integrity.
    - Implement model methods for slug generation and tag management.
    - Introduce serializers for handling tag lists in API requests.
    - Develope utility functions for parsing and editing tag strings.
    - Add views for listing objects by tags and managing tag merging in the admin interface.
    - Includ HTML templates for tag merging functionality.

--- artoon2d_blog/views.py
+++ artoon2d_blog/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+# Import Django's generic class-based views for listing and detail display
+from django.views.generic import ListView, DetailView
+# Import the Post model from the current app's models.py
+from .models import Post
 
-# Create your views here.
-def home(request):
-    return render(request, 'artoon2d_blog/home.html', {})
+# View to display a list of blog posts
+class PostListView(ListView):
+    # Specify the model this view will operate on
+    model = Post
+    # Define the template to render the list of posts
+    template_name = 'blog/post_list.html'
+    # In the template, use {{ posts }} to access the list
+    context_object_name = 'posts'
+
+# View to display a single blog post
+class PostDetailView(DetailView):
+    # Specify the model this view will operate on
+    model = Post
+    # Define the template to render the detailed view of a single post
+    template_name = 'blog/post_detail.html'
