554038c303c307d2eee6b38695287f936bed06a2
hearthdesign <hearthxdesign@gmail.com>
2025-08-31 03:45:12 +0200

    Add tagging functionality with django-taggit

--- artoon2d_blog/models.py
+++ artoon2d_blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User # Import the User model for the author field
 
+from taggit.managers import TaggableManager # Import TaggableManager for tagging functionality
+
+''' Model to rapresent a blog post'''
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -8,8 +11,8 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     featured_image = models.ImageField(upload_to='post_images/', blank=True, null=True)
-    theme = models.CharField(max_length=50)
-  
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True) 
+    tags = TaggableManager() # Add tagging functionality 
     def __str__(self):
 	    return self.title + ' | ' + str(self.author)
 
