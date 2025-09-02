9a3ff3331d8ada8ac9970e098ee64c76b436e64b
hearthdesign <hearthxdesign@gmail.com>
2025-09-02 13:48:45 +0200

    Refactor: update database credentials in .env, enhance post list and detail views with search, filtering, and pagination features, and create base template for consistent layout

--- artoon2d/urls.py
+++ artoon2d/urls.py
@@ -1,24 +1,12 @@
-"""
-URL configuration for artoon2d project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
+from artoon2d_blog.views import PostListView
 
 urlpatterns = [
+    # Admin site
     path('admin/', admin.site.urls),
+    # include the blog app' s URLs
     path('', include('artoon2d_blog.urls')),
+    # Add a URL pattern for the postlist view
+    path('posts/', PostListView.as_view(), name='post_list'),
 ]
--- artoon2d_blog/views.py
+++ artoon2d_blog/views.py
@@ -1,21 +1,53 @@
-from django.shortcuts import render
-# Import Django's generic class-based views for listing and detail display
-from django.views.generic import ListView, DetailView
+from datetime import timedelta
+from django.utils.timezone import now
 # Import the Post model from the current app's models.py
 from .models import Post
+# import Q for complex queries
+from django.db.models import Q
+from django.views.generic import ListView, DetailView
 
-# View to display a list of blog posts
+'''View to display a list of blog posts with search and sorting functionality'''
 class PostListView(ListView):
-    # Specify the model this view will operate on
     model = Post
-    # Define the template to render the list of posts
     template_name = 'artoon2d_blog/post_list.html'
-    # In the template, use {{ posts }} to access the list
     context_object_name = 'posts'
+    paginate_by = 5
+    # Override get queryset to add search and filtering function
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        category = self.request.GET.get('category')
+        recent_days = self.request.GET.get('recent_days')
+        qs = Post.objects.all()
+        # Filter by recent days if defined and valid
+        if recent_days:
+            try:
+                days = int(recent_days)
+                cutoff = now() - timedelta(days=days)
+                qs = qs.filter(created_at__gte=cutoff)
+            except ValueError:
+                pass  # ignore invalid input
+        # Filter by search query in title, content or tags
+        if query:
+            qs = qs.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query) |
+                Q(tags__name__icontains=query)
+            ).distinct()
+        # filter by category if defined
+        if category:
+            qs = qs.filter(category__name__iexact=category)
+        # order by most recent posts first
+        return qs.order_by('-created_at')
 
-# View to display a single blog post
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.request.GET.get('q')
+        context['category'] = self.request.GET.get('category')
+        context['recent_days'] = self.request.GET.get('recent_days')
+        return context
+
+    ''' View to display the view of a single post'''
 class PostDetailView(DetailView):
-    # Specify the model this view will operate on
     model = Post
-    # Define the template to render the detailed view of a single post
     template_name = 'artoon2d_blog/post_detail.html'
+    context_object_name = 'post'
