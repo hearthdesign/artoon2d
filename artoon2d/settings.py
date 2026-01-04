8144b9bdd1187229353ebba09aab0627255e380c
hearthdesign <hearthxdesign@gmail.com>
2026-01-04 02:08:58 +0100

    Update: switch to using config for Cloudinary storage credentials

--- artoon2d/settings.py
+++ artoon2d/settings.py
@@ -121,10 +121,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = { 
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'), 
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'), 
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'), 
-    }
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'), 
+    'API_KEY': config('CLOUDINARY_API_KEY'), 
+    'API_SECRET': config('CLOUDINARY_API_SECRET'), 
+}
 
 # Media files (uploaded by users)
 MEDIA_URL = '/media/'
