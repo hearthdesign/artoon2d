9afe9c48ce52e276384941205a1864e40d949d8b
hearthdesign <hearthxdesign@gmail.com>
2026-01-07 16:11:05 +0100

    Update: modify Category model slug field to allow null values

--- artoon2d_blog/models.py
+++ artoon2d_blog/models.py
@@ -8,7 +8,7 @@ from django.utils.text import slugify
 # -------------------------------------------------
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=120, unique=True, blank=True, null=False)
+    slug = models.SlugField(max_length=120, unique=True, blank=True, null=True)
 
     class Meta:
         verbose_name_plural = "Categories"
