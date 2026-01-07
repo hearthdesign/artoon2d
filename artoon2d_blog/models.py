8657d411564a00f12c6b9be3e011b30c5e53a31b
hearthdesign <hearthxdesign@gmail.com>
2026-01-07 15:56:46 +0100

    Update: modify Category model to allow slug field to be blank and auto-generate unique slugs on save

--- artoon2d_blog/models.py
+++ artoon2d_blog/models.py
@@ -8,11 +8,22 @@ from django.utils.text import slugify
 # -------------------------------------------------
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(max_length=120, unique=True, blank=True, null=True)
 
     class Meta:
         verbose_name_plural = "Categories"
         ordering = ['name']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.name)
+            slug = base_slug
+            counter = 1
+            while Category.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
